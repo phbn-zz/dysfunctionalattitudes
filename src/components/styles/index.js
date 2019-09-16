76ee94c1ff87de5c805ddc606355135adaaafb51
Peter Bay <peterherborgbay@gmail.com>
2019-09-16 18:24:43 +0200

    initial comit

---
+++ src/components/styles/index.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  background-color: #282c34;
+  height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  background-color: #282c34;
+  margin-top: 10vh;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: 50px;
+  text-align: center;
+  justify-items: center;
+  justify-content: center;
+  margin: 24px 0;
+  padding-bottom: 24px;
+`;
